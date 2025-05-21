bc70dd521042dfbc72871193405b58f4243201ec
zypgungor <168725485+Zypgungorr@users.noreply.github.com>
2025-05-21 18:22:53 +0300

    Add Footer component to Home, Hakkımızda, Hizmetlerimiz, İletişim, and Kariyer pages; enhance layout and structure

--- app/kariyer/page.tsx
+++ app/kariyer/page.tsx
@@ -1,4 +1,21 @@
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
 export default function HakkimizdaPage() {
-    return <h1 className="text-4xl font-bold text-center mt-10">Kariyer Sayfası</h1>;
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      {/* Hikayemiz Section */}
+      <section className="relative h-screen bg-gray-800 text-white">
+       
+      </section>
+
+      {/* Hakkımızda Section */}
+      <section className="relative min-h-screen bg-[#9B1B30] text-white">
+       
+      </section>
+      <Footer/>
+    </div>
+  );
   }
   
